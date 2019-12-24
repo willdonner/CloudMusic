d2720ca1188ecb4bbae7483de74f51dc2ae33e2e
lichunkaik <57978568+lichunkaik@users.noreply.github.com>
2019-12-24 10:35:21 +0800

    添加搜索历史

--- app/src/main/java/com/dongxun/lichunkai/cloudmusic/Activity/SearchActivity.java
+++ app/src/main/java/com/dongxun/lichunkai/cloudmusic/Activity/SearchActivity.java
@@ -1,19 +1,30 @@
 package com.dongxun.lichunkai.cloudmusic.Activity;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DefaultItemAnimator;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.StaggeredGridLayoutManager;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.dongxun.lichunkai.cloudmusic.Adapter.SearcHistoryAdapter;
 import com.dongxun.lichunkai.cloudmusic.Adapter.SearchAdapter;
 import com.dongxun.lichunkai.cloudmusic.Bean.Song;
 import com.dongxun.lichunkai.cloudmusic.Common.Common;
@@ -27,7 +38,10 @@ import org.json.JSONObject;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import okhttp3.Call;
 import okhttp3.Callback;
@@ -40,13 +54,21 @@ public class SearchActivity extends AppCompatActivity implements View.OnClickLis
 
     private static final String TAG = "SearchActivity";
     private List<Song> songList = new ArrayList<>();//搜索结果
+    private ArrayList<String> searchHistory = new ArrayList<>();//搜索历史
 
     private EditText editText_search;
     private TextView textView_cancel;
     private RecyclerView recyclerView_result;
+    private ImageView imageView_deleteHistory;
+    private RecyclerView recyclerView_history;
+    private LinearLayout LinearLayout_history;
+    private ImageView imageView_waiting;
 
     private SearchAdapter searchAdapter;
     private LinearLayoutManager layoutManager;
+
+    private SearcHistoryAdapter searcHistoryAdapter;
+    private StaggeredGridLayoutManager layoutManager_history;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -63,6 +85,38 @@ public class SearchActivity extends AppCompatActivity implements View.OnClickLis
      * 初始化适配器
      */
     private void initAdapter() {
+        /**
+         * 搜索历史Adapter
+         */
+        searchHistory = getSearchHistory();
+        layoutManager_history = new StaggeredGridLayoutManager(1,StaggeredGridLayoutManager.HORIZONTAL);//瀑布流
+        recyclerView_history.setLayoutManager(layoutManager_history);
+        searcHistoryAdapter = new SearcHistoryAdapter(searchHistory);
+        recyclerView_history.setAdapter(searcHistoryAdapter);
+        //点击事件(实现自定义的点击事件接口)
+        searcHistoryAdapter.setOnItemClickListener(new SearcHistoryAdapter.OnItemClickListener() {
+            @Override
+            public void onClick(int position) {
+                //搜索选取历史
+                editText_search.setText(searchHistory.get(position));
+                editText_search.clearFocus();
+                //搜索
+                search(editText_search.getText().toString());
+                //关闭输入法
+                InputMethodManager m=(InputMethodManager) getSystemService(getBaseContext().INPUT_METHOD_SERVICE);
+                m.toggleSoftInput(0, InputMethodManager.HIDE_NOT_ALWAYS);
+                //存储搜索历史
+                saveSearchHistory(editText_search.getText().toString());
+                //隐藏历史布局
+                LinearLayout_history.setVisibility(View.GONE);
+            }
+        });
+        //没有搜索历史隐藏布局
+        if (searchHistory.size() == 0) LinearLayout_history.setVisibility(View.GONE);
+
+        /**
+         * 搜索结果Adapter
+         */
         layoutManager = new LinearLayoutManager(this);
         recyclerView_result.setLayoutManager(layoutManager);
         searchAdapter = new SearchAdapter(songList);
@@ -87,6 +141,72 @@ public class SearchActivity extends AppCompatActivity implements View.OnClickLis
         });
     }
 
+    /**
+     * 读取搜索历史
+     */
+    private ArrayList<String> getSearchHistory() {
+        ArrayList<String> history = new ArrayList<>();//搜索历史
+        SharedPreferences preferences = getSharedPreferences("data",MODE_PRIVATE);
+        String str_searchHistory = preferences.getString("searchHistory","");
+        if (str_searchHistory.length() > 0){
+            if (str_searchHistory.trim().charAt(0) == ',')  str_searchHistory = str_searchHistory.substring(1).trim();
+            for (String str:str_searchHistory.split(",")){
+                history.add(str);
+            }
+        }
+        //倒叙
+        Collections.reverse(history);
+        return history;
+    }
+
+    /**
+     * 保存搜索历史
+     * @param keyword
+     */
+    private void saveSearchHistory(String keyword) {
+        String str_searchHistory = "";
+        Collections.reverse(searchHistory);
+        if (searchHistory.contains(keyword)) {
+            //搜索历史包含该关键词，放至最前
+            int index = searchHistory.indexOf(keyword);
+            searchHistory.remove(keyword);
+        }
+        for (String his:searchHistory){
+            if (!his.trim().equals("")) str_searchHistory = str_searchHistory + "," + his;
+        }
+        str_searchHistory = str_searchHistory + "," + keyword;
+
+        SharedPreferences.Editor editor = getSharedPreferences("data",MODE_PRIVATE).edit();
+        editor.putString("searchHistory",str_searchHistory);
+        editor.apply();
+    }
+
+    /**
+     * 删除搜索历史
+     */
+    private void deleteSearchHistory() {
+        if (getSearchHistory().size() == 0) return;
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("提示");
+        builder.setMessage("确认删除搜索历史？");
+        builder.setPositiveButton("确定", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                searchHistory.remove(searchHistory);
+                SharedPreferences.Editor editor = getSharedPreferences("data",MODE_PRIVATE).edit();
+                editor.putString("searchHistory","");
+                editor.apply();
+                //刷新UI
+                LinearLayout_history.setVisibility(View.GONE);
+            }
+        });
+        builder.setNegativeButton("取消", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+            }
+        });
+        builder.show();
+    }
 
     /**
      * 显示键盘
@@ -104,6 +224,25 @@ public class SearchActivity extends AppCompatActivity implements View.OnClickLis
     private void initView() {
         //监听输入法点击搜索键，搜索歌曲
         editText_search = findViewById(R.id.editText_search);
+        editText_search.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+            @Override
+            public void afterTextChanged(Editable s) {
+                //文本为空显示搜索历史
+                if (editText_search.getText().toString().length() == 0){
+                    recyclerView_result.setVisibility(View.GONE);
+                    LinearLayout_history.setVisibility(View.VISIBLE);
+                    searchHistory.removeAll(searchHistory);
+                    searchHistory.addAll(getSearchHistory());
+                    searcHistoryAdapter.notifyDataSetChanged();
+                }
+            }
+        });
         editText_search.setOnEditorActionListener(new TextView.OnEditorActionListener() {
             @Override
             public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
@@ -115,6 +254,10 @@ public class SearchActivity extends AppCompatActivity implements View.OnClickLis
                         //关闭输入法
                         InputMethodManager m=(InputMethodManager) getSystemService(getBaseContext().INPUT_METHOD_SERVICE);
                         m.toggleSoftInput(0, InputMethodManager.HIDE_NOT_ALWAYS);
+                        //存储搜索历史
+                        saveSearchHistory(keyword);
+                        //隐藏历史布局
+                        LinearLayout_history.setVisibility(View.GONE);
                     }else {
                         //显示其余组件
                     }
@@ -126,6 +269,12 @@ public class SearchActivity extends AppCompatActivity implements View.OnClickLis
         textView_cancel = findViewById(R.id.textView_cancel);
         textView_cancel.setOnClickListener(this);
         recyclerView_result = findViewById(R.id.recyclerView_result);
+        imageView_deleteHistory = findViewById(R.id.imageView_deleteHistory);
+        imageView_deleteHistory.setOnClickListener(this);
+        recyclerView_history = findViewById(R.id.recyclerView_history);
+        LinearLayout_history = findViewById(R.id.LinearLayout_history);
+        imageView_waiting = findViewById(R.id.imageView_waiting);
+        imageView_waiting.setVisibility(View.GONE);
     }
 
     /**
@@ -142,6 +291,8 @@ public class SearchActivity extends AppCompatActivity implements View.OnClickLis
      * @param keyWord 关键词
      */
     private void search(final String keyWord) {
+        imageView_waiting.setVisibility(View.VISIBLE);
+        recyclerView_result.setVisibility(View.GONE);
         new Thread(new Runnable() {
             @Override
             public void run() {
@@ -202,6 +353,8 @@ public class SearchActivity extends AppCompatActivity implements View.OnClickLis
             runOnUiThread(new Runnable() {
                 @Override
                 public void run() {
+                    imageView_waiting.setVisibility(View.GONE);
+                    recyclerView_result.setVisibility(View.VISIBLE);
                     //回到顶部并应用更改
                     layoutManager.scrollToPosition(0);
                     layoutManager.setStackFromEnd(true);
@@ -223,7 +376,12 @@ public class SearchActivity extends AppCompatActivity implements View.OnClickLis
             case R.id.textView_cancel:
                 finish();
                 break;
+            case R.id.imageView_deleteHistory:
+                //删除搜索历史
+                deleteSearchHistory();
+                break;
 
         }
     }
+
 }
