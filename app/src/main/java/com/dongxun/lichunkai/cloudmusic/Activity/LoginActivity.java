9e9a341f7f54a16615427db7c206144a038b2c65
lichunkaik <57978568+lichunkaik@users.noreply.github.com>
2019-12-27 17:34:56 +0800

    登录页

--- app/src/main/java/com/dongxun/lichunkai/cloudmusic/Activity/LoginActivity.java
+++ app/src/main/java/com/dongxun/lichunkai/cloudmusic/Activity/LoginActivity.java
@@ -4,21 +4,36 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.dongxun.lichunkai.cloudmusic.PopWindow.LoginWindow;
 import com.dongxun.lichunkai.cloudmusic.R;
+import com.dongxun.lichunkai.cloudmusic.Util.PhoneFormatCheckUtils;
+import com.dongxun.lichunkai.cloudmusic.Util.ToolHelper;
 import com.gyf.immersionbar.ImmersionBar;
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private ImageView imageView_phone;
-    private ImageView imageView_email;
+    private ImageView imageView_back;
+    private Button button_next;
+    private LinearLayout LinearLayout_account;
+    private LinearLayout LinearLayout_password;
+    private EditText editText_password;
+    private EditText editText_account;
+    private Button button_login;
+
+    private String account;
+    private String password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,10 +57,16 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
      * 初始化组件
      */
     private void initView() {
-        imageView_phone = findViewById(R.id.imageView_phone);
-        imageView_phone.setOnClickListener(this);
-        imageView_email = findViewById(R.id.imageView_email);
-        imageView_email.setOnClickListener(this);
+        imageView_back = findViewById(R.id.imageView_back);
+        imageView_back.setOnClickListener(this);
+        button_next = findViewById(R.id.button_next);
+        button_next.setOnClickListener(this);
+        LinearLayout_account = findViewById(R.id.LinearLayout_account);
+        LinearLayout_password = findViewById(R.id.LinearLayout_password);
+        editText_password = findViewById(R.id.editText_password);
+        editText_account = findViewById(R.id.editText_account);
+        button_login = findViewById(R.id.button_login);
+        button_login.setOnClickListener(this);
     }
 
     /**
@@ -55,18 +76,50 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     @Override
     public void onClick(View v) {
         switch (v.getId()){
-            case R.id.imageView_phone:
-                //电话号码登录
-                Toast.makeText(this,"电话号码登录",Toast.LENGTH_SHORT).show();
-                LoginWindow loginWindow_phone = new LoginWindow(this,"电话号码");
-                loginWindow_phone.show();
+            case R.id.imageView_back:
+                if (LinearLayout_account.getVisibility() == View.GONE) {
+                    LinearLayout_account.setVisibility(View.VISIBLE);
+                    LinearLayout_password.setVisibility(View.GONE);
+                    editText_account.setText(account);
+                    password = "";
+                    return;
+                }else finish();
                 break;
-            case R.id.imageView_email:
-                //电子邮箱登录
-                Toast.makeText(this,"电子邮箱登录",Toast.LENGTH_SHORT).show();
-                LoginWindow loginWindow_email = new LoginWindow(this,"邮箱");
-                loginWindow_email.show();
+            case R.id.button_next:
+                //验证电话号码
+                account = editText_account.getText().toString().trim();
+                if (PhoneFormatCheckUtils.isPhoneLegal(account)){
+                    Log.e("TAG", "onClick: "+account );
+                    LinearLayout_account.setVisibility(View.GONE);
+                    LinearLayout_password.setVisibility(View.VISIBLE);
+                }else {
+                 Toast.makeText(this,"请输入正确的手机号！",Toast.LENGTH_SHORT).show();
+                }
                 break;
+            case R.id.button_login:
+                //登录
+                if (editText_password.getText().toString().trim().equals("")){
+                    Toast.makeText(this,"请填写密码！",Toast.LENGTH_SHORT).show();
+                }else {
+                    Toast.makeText(this,"登录！",Toast.LENGTH_SHORT).show();
+                    button_login.setText("登录中...");
+                    //等待提示框
+
+                }
+                break;
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (LinearLayout_account.getVisibility() == View.GONE) {
+            LinearLayout_account.setVisibility(View.VISIBLE);
+            LinearLayout_password.setVisibility(View.GONE);
+            editText_account.setText(account);
+            password = "";
+            return;
+        }else {
+            super.onBackPressed();
         }
     }
 }
--- app/src/main/java/com/dongxun/lichunkai/cloudmusic/Activity/LunchActivity.java
+++ app/src/main/java/com/dongxun/lichunkai/cloudmusic/Activity/LunchActivity.java
@@ -1,72 +1,101 @@
 package com.dongxun.lichunkai.cloudmusic.Activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
-import android.app.Activity;
+import android.Manifest;
 import android.content.Intent;
-import android.os.Build;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
-import android.view.WindowManager;
 import android.view.animation.AlphaAnimation;
 import android.view.animation.Animation;
+import android.view.animation.TranslateAnimation;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.dongxun.lichunkai.cloudmusic.Dialog.permissionDialog;
 import com.dongxun.lichunkai.cloudmusic.R;
+import com.dongxun.lichunkai.cloudmusic.Util.PermissionUtil;
+import com.gyf.immersionbar.BarHide;
+import com.gyf.immersionbar.ImmersionBar;
 
 import static android.os.SystemClock.sleep;
 
-public class LunchActivity extends AppCompatActivity implements Animation.AnimationListener {
+public class LunchActivity extends AppCompatActivity implements Animation.AnimationListener, View.OnClickListener {
 
-    AlphaAnimation alphaAnimation;
+    private ImageView imageView_logo;
+    private TextView textView_name;
+    private Button button_phoneLogin;
 
+    private AlphaAnimation alphaAnimation_appear;//logo及下方字体透明度动画（出现）
+    private AlphaAnimation alphaAnimation_hide;//logo及下方字体透明度动画（消失）
+    private TranslateAnimation translateAnimation_logo;//logo平移动画
+    private AlphaAnimation alphaAnimation_loginAppear;//登录透明度动画（出现）
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate( savedInstanceState );
         setContentView(R.layout.activity_lunch);
-        setTranslucent(this);
-        alphaAnimation();
+        initBar();
+        initView();
+        initAnimation();
+        //透明度动画
+        startAnimation();
     }
 
     /**
-     * 透明度动画
+     * 初始化组件
      */
-    private void alphaAnimation() {
-        ImageView imageView = findViewById(R.id.imageView);
-        TextView textView = findViewById(R.id.textView);
-        //设置透明度变化
-        alphaAnimation = new AlphaAnimation(0.0f, 1.0f);//第一个参数开始的透明度，第二个参数结束的透明度
-        alphaAnimation.setDuration(500);//多长时间完成这个动作
-        alphaAnimation.setAnimationListener(this);
-        //开始
-        imageView.startAnimation(alphaAnimation);
-        textView.startAnimation(alphaAnimation);
+    private void initView() {
+        imageView_logo = findViewById(R.id.imageView_logo);
+        textView_name = findViewById(R.id.textView_name);
+        button_phoneLogin = findViewById(R.id.button_phoneLogin);
+        button_phoneLogin.setOnClickListener(this);
     }
 
     /**
-     * 隐藏状态栏标题栏
-     * @param activity
+     * 初始化动画
      */
-    public void setTranslucent(Activity activity) {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-            // 设置状态栏透明
-            activity.getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
-            //隐藏标题栏
-            getSupportActionBar().hide();
-        }
+    private void initAnimation() {
+        //透明度（显示）
+        alphaAnimation_appear = new AlphaAnimation(0.0f, 1.0f);//第一个参数开始的透明度，第二个参数结束的透明度
+        alphaAnimation_appear.setDuration(500);//多长时间完成这个动作
+        alphaAnimation_appear.setAnimationListener(this);
+        //透明度（隐藏）
+        alphaAnimation_hide = new AlphaAnimation(1.0f, 0.0f);//第一个参数开始的透明度，第二个参数结束的透明度
+        alphaAnimation_hide.setDuration(500);//多长时间完成这个动作
+        alphaAnimation_hide.setAnimationListener(this);
+        //logo平移动画
+        translateAnimation_logo = new TranslateAnimation(0,0, 0, -500);//前两个参数是设置x轴的起止位置，后两个参数设置y轴的起止位置
+        translateAnimation_logo.setDuration(500);
+        translateAnimation_logo.setFillAfter(true);
+        //登录模块透明度（显示）
+        alphaAnimation_loginAppear = new AlphaAnimation(0.0f, 1.0f);//第一个参数开始的透明度，第二个参数结束的透明度
+        alphaAnimation_loginAppear.setDuration(800);//多长时间完成这个动作
+        alphaAnimation_loginAppear.setAnimationListener(this);
     }
 
     /**
-     * 跳转登录
+     * 初始化标题栏状态栏
      */
-    public void goLogin() {
-        Intent intent = new Intent(LunchActivity.this,MainActivity.class);
-        startActivity(intent);
-        finish();
+    private void initBar() {
+        //隐藏标题栏
+        getSupportActionBar().hide();
+        //隐藏状态栏
+        ImmersionBar.with(this).hideBar(BarHide.FLAG_HIDE_BAR).init();
+    }
+
+    /**
+     * 启动动画
+     */
+    private void startAnimation() {
+        //启动动画
+        imageView_logo.startAnimation(alphaAnimation_appear);
+        textView_name.startAnimation(alphaAnimation_appear);
     }
 
     /**
@@ -75,49 +104,113 @@ public class LunchActivity extends AppCompatActivity implements Animation.Animat
      */
     @Override
     public void onAnimationStart(Animation animation) {
-        //开始
     }
-
     @Override
     public void onAnimationEnd(Animation animation) {
         //结束
-        if (animation.equals(alphaAnimation)){
-            new Thread( new Runnable( ) {
-                @Override
-                public void run() {
-                    //耗时任务，比如加载网络数据
-                    runOnUiThread(new Runnable() {
-                        @Override
-                        public void run() {
-                            // 这里可以睡几秒钟，如果要放广告的话
-                            sleep(500);
-                            //权限申请
+        if (animation.equals(alphaAnimation_appear)){   //logo及name显示
+            showPermissionDialog();
+        }
+        if (animation.equals(alphaAnimation_hide)){   //name消失
+            textView_name.setVisibility(View.GONE);
+        }
+        if (animation.equals(alphaAnimation_loginAppear)){   //logo平移
+            button_phoneLogin.setVisibility(View.VISIBLE);
+        }
+    }
+
+    @Override
+    public void onAnimationRepeat(Animation animation) {
+
+    }
+
+    /**
+     * 权限
+     * @param requestCode
+     * @param permissions
+     * @param grantResults
+     */
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode){
+            case 502:
+                if(grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    hideLogo();
+                }else {
+                    Toast.makeText(this,"用户拒绝了权限申请",Toast.LENGTH_SHORT).show();
+                    finish();
+                }
+                break;
+        }
+    }
+
+    /**
+     * 移动logo及其隐藏Name
+     */
+    public void hideLogo() {
+        //显示状态栏
+        ImmersionBar.with(this).reset().init();
+        //下方字体消失动画
+        textView_name.startAnimation(alphaAnimation_hide);
+        //logo平移动画
+        imageView_logo.startAnimation(translateAnimation_logo);
+        //登录按钮出现
+        button_phoneLogin.startAnimation(alphaAnimation_loginAppear);
+    }
+
+    /**
+     * 获取权限对话框
+     */
+    public void showPermissionDialog() {
+        new Thread( new Runnable( ) {
+            @Override
+            public void run() {
+                //耗时任务，比如加载网络数据
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        // 这里可以睡几秒钟，如果要放广告的话
+                        sleep(500);
+                        //权限申请
+                        if (ContextCompat.checkSelfPermission(LunchActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                                != PackageManager.PERMISSION_GRANTED){
                             new permissionDialog(LunchActivity.this).builder().setTitle("云音乐权限申请")
                                     .setMsg("云音乐需要获取存储空间和设备信息权限，以保证歌曲正常播放下载以及您的帐号安全。")
                                     .setCancelable(false)
                                     .setPositiveButton("授权", new View.OnClickListener() {
                                         @Override
                                         public void onClick(View v) {
-                                            //授权
-                                            Toast.makeText(LunchActivity.this,"授权",Toast.LENGTH_SHORT).show();
-                                            
+                                            //获取权限
+                                            PermissionUtil.getInstance().requestSD(LunchActivity.this);
                                         }
                                     }).setNegativeButton("取消", new View.OnClickListener() {
-                                        @Override
-                                        public void onClick(View v) {
-                                            //退出
-                                            finish();
-                                        }
-                                    }).show();
+                                @Override
+                                public void onClick(View v) {
+                                    //退出
+                                    finish();
+                                }
+                            }).show();
+                        }else {
+                            hideLogo();
                         }
-                    });
-                }
-            } ).start();
-        }
+                    }
+                });
+            }
+        } ).start();
     }
 
     @Override
-    public void onAnimationRepeat(Animation animation) {
-        //重复
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.button_phoneLogin:
+                Intent intent = new Intent(LunchActivity.this,LoginActivity.class);
+                startActivity(intent);
+                break;
+        }
     }
 }
