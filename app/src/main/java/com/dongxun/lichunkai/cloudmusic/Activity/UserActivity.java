9ef447c4007f3089f38a092872569b32fae4d579
lichunkaik <57978568+lichunkaik@users.noreply.github.com>
2020-01-13 11:18:45 +0800

    获取用户详情及其部分信息呈现

--- app/src/main/java/com/dongxun/lichunkai/cloudmusic/Activity/UserActivity.java
+++ app/src/main/java/com/dongxun/lichunkai/cloudmusic/Activity/UserActivity.java
@@ -3,35 +3,75 @@ package com.dongxun.lichunkai.cloudmusic.Activity;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.animation.TranslateAnimation;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.dongxun.lichunkai.cloudmusic.Adapter.MainPagerAdapter;
+import com.dongxun.lichunkai.cloudmusic.Bean.Song;
+import com.dongxun.lichunkai.cloudmusic.Class.CircleImageView;
+import com.dongxun.lichunkai.cloudmusic.Class.ResizableImageView;
+import com.dongxun.lichunkai.cloudmusic.Common.Common;
+import com.dongxun.lichunkai.cloudmusic.LocalBroadcast.SendLocalBroadcast;
 import com.dongxun.lichunkai.cloudmusic.R;
+import com.dongxun.lichunkai.cloudmusic.Util.ToolHelper;
 import com.gyf.immersionbar.ImmersionBar;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 /**
  * 用户主页（自己和其他用户）
  */
-public class UserActivity extends AppCompatActivity {
+public class UserActivity extends AppCompatActivity implements View.OnClickListener {
 
     private String TAG = "UserActivity";
+
+    private ResizableImageView ResizableImageView_background;
+    private CircleImageView CircleImageView_head;
+    private TextView textView_nickname;
+    private TextView textView_follow;
+    private TextView textView_fans;
+    private TextView textView_years;
+    private TextView textView_level;
+    private TextView textView_momentCount;
 
     //viewpager相关
     private View view_index, view_moments;
     private ViewPager viewPager;  //对应的viewPager
     private List<View> viewList;//view数组
-
-    //view_index组件
+    private LinearLayout LinearLayout_index;
+    private LinearLayout LinearLayout_moment;
     private TextView textView_index;
     private TextView textView_moment;
+
+    //view_index组件
+    private TextView textView_listenCount;
+    private TextView textView_province;
+    private TextView textView_city;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,18 +81,188 @@ public class UserActivity extends AppCompatActivity {
         initStateBar();
         initView();
         setViewpager();
+        updateUI();
+        getUserInfo();
+    }
+
+    private void updateUI() {
+        textView_nickname.setText(Common.user.getNickname());
+        CircleImageView_head.setImageBitmap(Common.user.getAvatarUrl_bitmap());
+        ResizableImageView_background.setImageBitmap(Common.user.getBackgroundUrl_bitmap());
+    }
+
+    /**
+     * 获取用户信息
+     */
+    private void getUserInfo() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try{
+                    OkHttpClient client = new OkHttpClient();//新建一个OKHttp的对象
+                    Request request = new Request.Builder()
+                            .url("https://neteasecloudmusicapi.willdonner.top/user/detail?uid="+ Common.user.getUserId() +"")
+                            .build();
+                    Call call = client.newCall(request);
+                    call.enqueue(new Callback() {
+                        @Override
+                        public void onFailure(Call call, IOException e) {
+                        }
+                        @Override
+                        public void onResponse(Call call, Response response) throws IOException {
+                            final String responseData = response.body().string();//处理返回的数据
+                            Log.e(TAG, "onResponse: "+responseData);
+                            //处理JSON
+                            try {
+                                JSONObject newRes = new JSONObject(responseData);
+                                String code = newRes.getString("code");
+                                if (code.equals("200")){
+                                    Log.e(TAG, "用户详情获取成功");
+                                    final String level = newRes.getString("level");//等级
+                                    final String listenSongs = newRes.getString("listenSongs");//累计听歌
+                                    JSONObject profile = newRes.getJSONObject("profile");
+                                    final String nickname = profile.getString("nickname");//昵称
+                                    final String province = profile.getString("province");//省份
+                                    String vipType = profile.getString("vipType");//vip类型
+                                    final String birthday = profile.getString("birthday");//生日
+                                    String gender = profile.getString("gender");//性别
+                                    final String avatarUrl = profile.getString("avatarUrl");//头像URL
+                                    String userType = profile.getString("userType");//用户类型
+                                    final String city = profile.getString("city");//城市
+                                    final String backgroundUrl = profile.getString("backgroundUrl");//背景URl
+                                    String signature = profile.getString("signature");//个性签名
+                                    final String follows = profile.getString("follows");//关注
+                                    final String followeds = profile.getString("followeds");//粉丝
+                                    final String eventCount = profile.getString("eventCount");//动态数量
+
+                                    runOnUiThread(new Runnable() {
+                                        @Override
+                                        public void run() {
+                                            textView_momentCount.setText(eventCount);
+                                            textView_nickname.setText(nickname);
+                                            textView_follow.setText(follows);
+                                            textView_fans.setText(followeds);
+                                            textView_level.setText("Lv."+level);
+                                            textView_years.setText(ToolHelper.millisecondToDate(new Long(birthday)).substring(2,3)+"0后");//年代
+                                            //view_index组件UI
+                                            textView_listenCount.setText(listenSongs);
+                                            textView_province.setText(province);
+                                            textView_city.setText(city);
+
+
+                                        }
+                                    });
+
+                                    //头像
+                                    new Thread(new Runnable() {
+                                        @Override
+                                        public void run() {
+                                            URL imageurl = null;
+                                            try {
+                                                imageurl = new URL(avatarUrl);
+                                            } catch (MalformedURLException e) {
+                                                e.printStackTrace();
+                                            }
+                                            try {
+                                                HttpURLConnection conn = (HttpURLConnection)imageurl.openConnection();
+                                                conn.setDoInput(true);
+                                                conn.connect();
+                                                InputStream is = conn.getInputStream();
+                                                final Bitmap bitmap = BitmapFactory.decodeStream(is);
+                                                //切换主线程更新UI
+                                                CircleImageView_head.post(new Runnable() {
+                                                    @Override
+                                                    public void run() {
+                                                        CircleImageView_head.setImageBitmap(bitmap);
+                                                        Common.user.setAvatarUrl_bitmap(bitmap);
+                                                        Common.user.setAvatarUrl(avatarUrl);
+                                                    }
+                                                });
+                                                is.close();
+                                            } catch (IOException e) {
+                                                e.printStackTrace();
+                                            }
+                                        }
+                                    }).start();
+
+                                    //背景
+                                    new Thread(new Runnable() {
+                                        @Override
+                                        public void run() {
+                                            URL imageurl = null;
+                                            try {
+                                                imageurl = new URL(backgroundUrl);
+                                            } catch (MalformedURLException e) {
+                                                e.printStackTrace();
+                                            }
+                                            try {
+                                                HttpURLConnection conn = (HttpURLConnection)imageurl.openConnection();
+                                                conn.setDoInput(true);
+                                                conn.connect();
+                                                InputStream is = conn.getInputStream();
+                                                final Bitmap bitmap = BitmapFactory.decodeStream(is);
+                                                //切换主线程更新UI
+                                                ResizableImageView_background.post(new Runnable() {
+                                                    @Override
+                                                    public void run() {
+                                                        ResizableImageView_background.setImageBitmap(bitmap);
+                                                        Common.user.setBackgroundUrl_bitmap(bitmap);
+                                                        Common.user.setBackgroundUrl(backgroundUrl);
+                                                    }
+                                                });
+                                                is.close();
+                                            } catch (IOException e) {
+                                                e.printStackTrace();
+                                            }
+                                        }
+                                    }).start();
+
+
+                                }else {
+                                    Log.e(TAG, "用户详情获取失败");
+                                }
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    });
+                }catch (Exception e){
+                    e.printStackTrace();
+                }
+            }
+        }).start();
     }
 
     private void initView() {
+        ResizableImageView_background = findViewById(R.id.ResizableImageView_background);
+        ResizableImageView_background.setOnClickListener(this);
+        CircleImageView_head = findViewById(R.id.CircleImageView_head);
+        CircleImageView_head.setOnClickListener(this);
+        textView_nickname = findViewById(R.id.textView_nickname);
+        textView_follow = findViewById(R.id.textView_follow);
+        textView_fans = findViewById(R.id.textView_fans);
+        textView_years = findViewById(R.id.textView_years);
+        textView_level = findViewById(R.id.textView_level);
+        textView_momentCount = findViewById(R.id.textView_momentCount);
+
         //实例化view
         viewPager = findViewById(R.id.viewpager);
         LayoutInflater inflater=getLayoutInflater();
         view_index = inflater.inflate(R.layout.viewpager_user_index, null);
         view_moments = inflater.inflate(R.layout.viewpager_user_moments,null);
 
-        //view_index组件
+        //组件
         textView_index = findViewById(R.id.textView_index);
         textView_moment = findViewById(R.id.textView_moment);
+        LinearLayout_index = findViewById(R.id.LinearLayout_index);
+        LinearLayout_index.setOnClickListener(this);
+        LinearLayout_moment = findViewById(R.id.LinearLayout_moment);
+        LinearLayout_moment.setOnClickListener(this);
+
+        //view_index组件
+        textView_listenCount = view_index.findViewById(R.id.textView_listenCount);
+        textView_province = view_index.findViewById(R.id.textView_province);
+        textView_city = view_index.findViewById(R.id.textView_city);
     }
 
     /**
@@ -133,5 +343,25 @@ public class UserActivity extends AppCompatActivity {
         getSupportActionBar().hide();
 //        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);//黑色状态栏字体
         getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);//白色状态栏字体
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.LinearLayout_index:
+                viewPager.setCurrentItem(0);
+                break;
+            case R.id.LinearLayout_moment:
+                viewPager.setCurrentItem(1);
+                break;
+            case R.id.ResizableImageView_background:
+                //查看背景
+
+                break;
+            case R.id.CircleImageView_head:
+                //查看头像
+
+                break;
+        }
     }
 }
